88e3c191d295932129470bbb4bafecc92d7f4898
YJCMS <sen9563@gmail.com>
2025-01-05 20:17:35 +0900

    Create project

---
+++ src/main/java/com/eyeon/eyeonscraper/EyeOnScraperApplication.java
@@ -0,0 +1,13 @@
+package com.eyeon.eyeonscraper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EyeOnScraperApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EyeOnScraperApplication.class, args);
+    }
+
+}
